8e6fb8873cbc2ddb739655ab4094b4ad4cff6e88
luisanzaldo <luis.anzaldo@metarrelato.com>
2026-02-18 18:59:50 -0600

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://luisanzaldo.github.io',
+  base: '/rutadorada',
   vite: {
     plugins: [tailwindcss()]
   }
