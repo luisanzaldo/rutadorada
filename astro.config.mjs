f5b35d9e0caac591f5fcfad9d4db12bae53e5abc
luisanzaldo <luis.anzaldo@metarrelato.com>
2026-02-19 22:06:06 -0600

    Cambios en inicio de sesión

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luisanzaldo.github.io',
+  site: 'https://metarrelato.com',
   base: '/rutadorada',
   vite: {
     // @ts-ignore
