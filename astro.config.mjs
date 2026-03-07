17472672bd286d0f1fcd964753898a3b0be7a91c
luisanzaldo <luis.anzaldo@metarrelato.com>
2026-03-06 19:15:45 -0600

    Embeber de YT

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import remarkOembed from 'remark-oembed';
 
 export default defineConfig({
   site: 'https://www.rutadoradafilms.com',
   markdown: {
-    remarkPlugins: [
-      async () => {
-        const { default: remarkEmbedder } = await import('@remark-embedder/core');
-        const { default: oembedTransformer } = await import('@remark-embedder/transformer-oembed');
-        return remarkEmbedder.default({ transformers: [oembedTransformer] });
-      }
-    ],
+    remarkPlugins: [remarkOembed],
   },
   vite: {
     plugins: [tailwindcss()]
